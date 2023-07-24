2ff1143c72c000c9d035eb648084f09a67c348d6
Callmejhed <68906942+Callmejhed@users.noreply.github.com>
2023-07-24 04:56:33 -0700

    commit from terminal

--- src/Components/Hero/elements.js
+++ src/Components/Hero/elements.js
@@ -4,4 +4,10 @@ export const Wrapper = styled.section`
     display: flex;
     justify-content: space-around;
     flex-wrap: wrap-reverse;
+
+    & .textcontainer{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
 `
